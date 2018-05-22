567c2a5d5491b8d9becb9a14baca6be9bef97d33
uojo <friend1020@163.com>
2018-05-22 23:23:10 +0800

    save

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  // parser: 'babel-eslint',
+  parserOptions: {
+    "ecmaVersion": 6,
+    "sourceType": 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  plugins: [
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'comma-dangle': ['error', 'only-multiline'],
+  }
+}
