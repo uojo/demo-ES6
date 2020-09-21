4ac46dabb1d7002bc1d523da5d777e928e9a9f57
uojo <friend1020@163.com>
2020-09-21 09:51:09 +0800

    save

--- src/extends_func.js
+++ src/extends_func.js
@@ -4,7 +4,7 @@ if (0) {
   // 借用构造函数
   // 优点：构造时可以传参给父类
   // 缺点：没有继承父类原型对象内的属性、方法
-  function Animal (name) {
+  function Animal(name) {
     this.name = name
     this.colors = ['red', 'blue', 'green']
   }
@@ -13,7 +13,7 @@ if (0) {
     console.log(this.name)
   }
 
-  function Cat (name, age) {
+  function Cat(name, age) {
     // 继承属性。可以传参数
     Animal.call(this, name)
     this.age = age
@@ -22,13 +22,51 @@ if (0) {
   let obj1 = new Cat()
   console.log(obj1 instanceof Cat) // true
   console.log(obj1 instanceof Animal) // false。父类不在子类实例的原型链上
+  console.log(obj1.prototype) // undefined。没有原型对象
+}
+
+if (0) {
+  // 原型式继承
+  // 优点：简单（使用时，自己不必创建构造函数）
+  // 缺点：不能传参、不能共享属性
+
+  // Object.create(obj, extraProps)
+  function myExtend(obj) {
+    function Fn() {
+
+    }
+    Fn.prototype = obj
+    return new Fn()
+  }
+}
+
+if (0) {
+  // 寄生继承。简单且不考虑原型链
+  function createAnother(original) {
+    // 创建一个新对象，对象的原型指向父对象，这样实现了继承父对象的属性和方法。
+    var clone = Object.create(original)
+    // clone.__proto__ === original
+    // 为新对象添加自定义属性
+    clone.sayHi = function () {
+      console.log('hi')
+    }
+    return clone
+  }
+
+  var animal = {
+    name: 'james'
+  }
+
+  var cat = createAnother(animal)
+  // cat.sayHi() // hi
+  // console.log(cat.__proto__) // animal{...}
 }
 
 if (0) {
   // 组合继承
   // 优点：同【借用构造函数】、继承了方法
   // 缺点：以父类的实例作为子类的原型对象，导致子类的原型对象被污染了。
-  function Animal (name) {
+  function Animal(name) {
     this.name = name
     this.colors = ['red', 'blue', 'green']
   }
@@ -37,7 +75,7 @@ if (0) {
     console.log(this.name)
   }
 
-  function Cat (name, age) {
+  function Cat(name, age) {
     // 继承属性。可以传参数
     Animal.call(this, name)
     this.age = age
@@ -55,45 +93,8 @@ if (0) {
 }
 
 if (0) {
-  // 原型式继承
-  // 优点：简单（使用时，自己不必创建构造函数）
-  // 缺点：不能传参、不能共享属性
-
-  // Object.create(obj, extraProps)
-  function myExtend (obj) {
-    function Fn () {
-
-    }
-    Fn.prototype = obj
-    return new Fn()
-  }
-}
-
-if (0) {
-  // 寄生继承。简单且不考虑原型链
-  function createAnother (original) {
-    // 创建一个新对象，对象的原型指向父对象，这样实现了继承父对象的属性和方法。
-    var clone = Object.create(original)
-    // clone.__proto__ === original
-    // 为新对象添加自定义属性
-    clone.sayHi = function () {
-      console.log('hi')
-    }
-    return clone
-  }
-
-  var animal = {
-    name: 'james'
-  }
-
-  var cat = createAnother(animal)
-  // cat.sayHi()
-  // console.log(cat.__proto__) // animal{...}
-}
-
-if (0) {
   // 寄生组合继承
-  function Animal (name) {
+  function Animal(name) {
     this.name = name
     this.colors = ['red', 'blue', 'green']
   }
@@ -102,17 +103,17 @@ if (0) {
     console.log(this.name)
   }
 
-  function Cat (name, age) {
+  function Cat(name, age) {
     // 继承属性。可以传参数
     Animal.call(this, name)
     this.age = age
   }
 
-  // 构造子类的原型对象。
-  function inheritPrototype (subType, superType) {
+  // 构造子类的原型对象
+  function inheritPrototype(subType, superType) {
     // 创建原型对象是超类原型对象的一个实例对象
     var prototype = Object.create(superType.prototype)
-    console.log('inheritPrototype -> prototype', prototype.__proto__ === superType.prototype)
+    // console.log('inheritPrototype -> prototype', prototype.__proto__ === superType.prototype)
     // 弥补因为重写原型而失去的默认的 constructor 属性。
     prototype.constructor = subType
     // 实现原型继承
